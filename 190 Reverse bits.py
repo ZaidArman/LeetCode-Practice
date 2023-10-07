2d2028650275fa65f906d4cb6a513e703deeb048
Zaid <zahidu864@gmai.com>
2023-10-07 09:52:26 -0700

    Add 190 problem of LeetCode

--- 190 Reverse bits.py
+++ 190 Reverse bits.py
@@ -1,5 +1,7 @@
 class Solution:
-    def reverseBits(self, n: int) -> int:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
         res = 0
         for i in range(32):
             bit = (n >> i) & 1
