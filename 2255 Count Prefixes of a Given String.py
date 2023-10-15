bfadfa1f7de7dced0bfb45b1593cdb3d46cad049
Zaid <zahidu864@gmai.com>
2023-10-15 10:00:27 -0700

    Add 2255 problem of LeetCode

---
+++ 2255 Count Prefixes of a Given String.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def countPrefixes(self, words, s):
+        
+        count = 0  # Initialize a counter to keep track of the number of matching prefixes.
+        for word in words:
+            if s.startswith(word):  # Check if the string s starts with the current word.
+                count += 1  # If it does, increment the count.
+
+        return count
