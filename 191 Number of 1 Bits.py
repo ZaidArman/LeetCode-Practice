70fa7b315d1528b3e5b0a6b75a83948177c6fe43
ZaidArman <79147841+ZaidArman@users.noreply.github.com>
2023-10-30 08:38:48 -0700

    Update 191 Number of 1 Bits.py

--- 191 Number of 1 Bits.py
+++ 191 Number of 1 Bits.py
@@ -7,6 +7,7 @@ Write a function that takes the binary representation of an unsigned integer and
 Input: n = 00000000000000000000000000001011
 Output: 3
 Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+
 # Example 2:
 Input: n = 00000000000000000000000010000000
 Output: 1
@@ -17,23 +18,14 @@ Input: n = 11111111111111111111111111111101
 Output: 31
 Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
 
-Constraints:
+# Constraints:
 The input must be a binary string of length 32.
 
-#Follow up: If this function is called many times, how would you optimize it?
+# Follow up: If this function is called many times, how would you optimize it?
 """
 
 class Solution(object):
     def hammingWeight(self, n):
-        """
-        Count the number of '1' bits (Hamming weight) in the binary representation of an unsigned integer.
-
-        :param n: The binary representation of the unsigned integer.
-        :type n: int
-
-        :return: The number of '1' bits in the binary representation.
-        :rtype: int
-        """
         count = 0
         while n:
             count += 1
